763f018e85711ff3b18c7c1dee348f8c888b6b0d
Piotr Solnica <piotr.solnica@gmail.com>
2020-12-22 06:46:49 +0100

    Extract test helpers and relation class to spec_helper

--- spec/integration/sql/compose_spec.rb
+++ spec/integration/sql/compose_spec.rb
@@ -1,27 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe SQL, ".compose" do
-  class UsersRelation
-    include SQL::Composer::NodeHelpers
-
-    def qualifier
-      sql_identifier(:users, backend: SQL::Composer.backends[:postgres])
-    end
-    alias_method :table, :qualifier
-
-    def id
-      sql_identifier(:id, qualifier: qualifier, backend: SQL::Composer.backends[:postgres])
-    end
-
-    def name
-      sql_identifier(:name, qualifier: qualifier, backend: SQL::Composer.backends[:postgres])
-    end
-  end
-
-  def compose(&block)
-    SQL.compose(args: UsersRelation.new, &block)
-  end
-
   let(:result) do
     query.to_s
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,32 @@ begin
 rescue LoadError
 end
 
+module Test
+  class UsersRelation
+    include SQL::Composer::NodeHelpers
+
+    def qualifier
+      sql_identifier(:users, backend: SQL::Composer.backends[:postgres])
+    end
+    alias_method :table, :qualifier
+
+    def id
+      sql_identifier(:id, qualifier: qualifier, backend: SQL::Composer.backends[:postgres])
+    end
+
+    def name
+      sql_identifier(:name, qualifier: qualifier, backend: SQL::Composer.backends[:postgres])
+    end
+  end
+
+  module Helpers
+    def compose(&block)
+      SQL.compose(args: UsersRelation.new, &block)
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.include(SQL::Composer::NodeHelpers)
+  config.include(Test::Helpers)
 end
